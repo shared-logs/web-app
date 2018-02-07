06794b66377dbb62dea7d6327f8dd17e5e4e2230
Seth Battis <seth@battis.net>
2018-02-06 21:00:48 -0500

    react-rich-markdown
    Geez. All I _really_ wanted was super-scripts!

--- src/views/entries/EntryViewer.js
+++ src/views/entries/EntryViewer.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Markdown from "react-markdown"
+import Markdown from "react-rich-markdown"
 
 export default class EntryViewer extends React.Component {
     render() {
@@ -7,7 +7,7 @@ export default class EntryViewer extends React.Component {
         if (entry) {
             return <div>
                     <h4>{entry.title} <small>{entry.created}</small></h4>
-                    <Markdown source={entry.detail}/>
+                    <Markdown source={entry.detail} options={{sh: false, math: false}}/>
                 <p><small>{entry.user.name}</small></p>
             </div>
         } else {
