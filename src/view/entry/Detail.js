06dee0e43bfcfcf6cb997d957e9a942303fb9bfa
Seth Battis <seth@battis.net>
2018-06-06 15:18:19 -0400

    Markdown is back
    Assuming it compiles on the server.

--- src/view/entry/Detail.js
+++ src/view/entry/Detail.js
@@ -1,6 +1,7 @@
 import React from "react"
-import Thumbnail from "../user/Thumbnail";
-import {humanDate} from "../utils";
+import Thumbnail from "../user/Thumbnail"
+import {humanDate} from "../utils"
+import Markdown from "react-markdown-it"
 
 export default class Detail extends React.Component {
     render() {
@@ -8,7 +9,7 @@ export default class Detail extends React.Component {
         return <div>
             <h4>{entry.title}</h4>
             <p><small>{humanDate(entry.created)}</small></p>
-            <p>{entry.detail}</p>
+            <Markdown source={entry.detail}/>
             {entry.user ? <Thumbnail user={entry.user} {...this.props}/> : ""}
             {entry.modified !== entry.created ? <p><small>Modified {humanDate(entry.modified)}</small></p> : ""}
         </div>
--- src/view/entry/Thumbnail.js
+++ src/view/entry/Thumbnail.js
@@ -1,6 +1,7 @@
 import React from "react"
 import LogThumbnail from "../log/Thumbnail"
 import UserThumbnail from "../user/Thumbnail"
+import Markdown from "react-markdown-it"
 import {humanDate} from "../utils";
 
 export default class Thumbnail extends React.Component {
@@ -13,7 +14,7 @@ export default class Thumbnail extends React.Component {
             {entry.log ? <div><LogThumbnail log={entry.log} {...this.props}/></div> : ""}
             <h5>{entry.title}</h5>
             <div><small>{humanDate(entry.created)}</small></div>
-            <p>{entry.detail.substr(0, Thumbnail.PREVIEW_LENGTH) + (entry.detail.length > Thumbnail.PREVIEW_LENGTH ? "..." : "")}</p>
+            <Markdown source={entry.detail.substr(0, Thumbnail.PREVIEW_LENGTH) + (entry.detail.length > Thumbnail.PREVIEW_LENGTH ? "..." : "")}/>q
             {entry.modified !== entry.created ? <div><small>Modified {humanDate(entry.modified)}</small></div> : ""}
             {entry.user ? <div><UserThumbnail user={entry.user} {...this.props}/></div> : ""}
         </div>
